e1e3f6979e14ef3844bc1a6923b0fb7ed8610a00
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-04 02:49:48 +0900

    create ItemController & routing

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Auth::routes();
 
-Route::get('/', function () {
-    return view('top');
-});
-
-
-// Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'ItemController@index')->name('item.index');
+Route::resource('item', 'ItemController')->except(['index']);
