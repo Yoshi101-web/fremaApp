87db676a9686a94fde641d73aabdf89a8e1beb51
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-08 14:01:57 +0900

    add create-view

---
+++ resources/views/item/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', '出品詳細')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <p>画像</p>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
