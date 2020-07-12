d859605524069f3e0e8b73be24784dd33d78bfa7
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-12 16:29:10 +0900

    Update Image.php

--- app/Image.php
+++ app/Image.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    public function items()
+    public function item()
     {
         return $this->BelongsTo('App\Item', 'item_id', 'id');
     }
