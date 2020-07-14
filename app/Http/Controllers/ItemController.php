4ddc3f777d708c0f136968fe5497f74128aeecce
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-14 22:55:49 +0900

    Add validation

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -53,6 +53,43 @@ class ItemController extends Controller
      */
     public function store(Request $request, Item $item)
     {
+        $rules = [
+            'item_name' => 'required|max:255',
+            'description' => 'required|max:6000',
+            'category_id' => 'required',
+            'category_children_id' => 'required',
+            'category_grand_children_id' => 'required',
+            'price' => 'required|min:300|max:9999999',
+            'condition' => 'required',
+            'shipping_fee_payer' => 'required',
+            'prefecture_id' => 'required',
+            'shipping_days' => 'required',
+        ];
+
+        $messages = [
+            'item_name.required' => '商品名を入力してください',
+            'item_name.max' => '商品名を255文字以下で入力してください',
+            'description.required' => '商品の説明を入力してください',
+            'description.max' => '商品の説明を6000文字以下で入力してください',
+            'category_id.required' => 'カテゴリーを選択してください',
+            'category_children_id.required' => 'カテゴリーを選択してください',
+            'category_grand_children_id.required' => 'カテゴリーを選択してください',
+            'price.required' => '価格を入力してください' ,
+            'price.min' => '価格を¥300以上で入力してください' ,
+            'price.max' => '価格を¥9999999以下で入力してください' ,
+            'condition.required' => '商品の状態を選択してください',
+            'shipping_fee_payer.required' => '配送料の負担を選択してください',
+            'prefecture_id.required' => '配送元の地域を選択してください',
+            'shipping_days.required' => '発送までの日数を選択してください',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            // return redirect('item.create')->withErrors($validator)->withInput();
+            return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+
         $latest = $item->orderBy('created_at', 'DESC')->first(); //最新のitemを取得
         $item_id = 1+$latest->id; ////最新のitemのidに１プラス。
         $images=array();  //からの配列を用意
